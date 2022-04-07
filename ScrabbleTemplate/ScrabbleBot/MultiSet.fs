865f9254548c06af76b8ff1a57e3497c3c1d3b0c
SadlifeCaw <caws@itu.dk>
2022-04-07 11:31:29 +0200

    Inserted MultiSet.fsi, Multiset.fs, StateMonad.fs

--- ScrabbleTemplate/ScrabbleBot/MultiSet.fs
+++ ScrabbleTemplate/ScrabbleBot/MultiSet.fs
@@ -2,8 +2,30 @@
 
 module internal MultiSet
 
-    type MultiSet<'a> = Temp of unit // Not implemented
+   type MultiSet<'a when 'a : comparison> = Map<'a, uint32> 
 
-    let empty : MultiSet<'a> = Temp () // Not implemented
-    let add   : 'a -> uint32 -> MultiSet<'a> -> MultiSet<'a> = fun _ _ _ -> failwith "Not implemented" 
-    let fold  : ('b -> 'a -> uint32 -> 'b) -> 'b -> MultiSet<'a> -> 'b = fun _ _ _ -> failwith "Not implemented"
+   let empty : MultiSet<'a> = Map.empty
+
+   let isEmpty (map: MultiSet<'a>) = map.IsEmpty
+
+   let size (s: MultiSet<'a>) : uint32 = Map.fold (fun x _ v -> x + v) 0u s
+
+   let contains a (s : MultiSet<'a>) = Map.exists (fun k _ ->  k = a) s
+
+   let numItems a (s : MultiSet<'a>) = if contains a s then s.[a] else 0u
+
+   let add a (n : uint32) (s : MultiSet<'a>) : MultiSet<'a> = s |> Map.add a ((numItems a s) + n)
+
+   let addSingle a (s : MultiSet<'a>) = s |> Map.add a ((numItems a s) + 1u)
+
+   let remove a n (s : MultiSet<'a>) : MultiSet<'a> = 
+       let value = numItems a s
+       if value >= n then Map.add a (value - n) s else Map.remove a s
+    
+   let removeSingle a (s : MultiSet<'a>) = remove a 1u s
+
+   let fold f acc (multiset : MultiSet<'b>) = Map.fold f acc multiset
+
+   let foldBack f (multiset : MultiSet<'b>) acc = Map.foldBack f multiset acc
+
+   let ofList lst = List.fold(fun acc element -> addSingle element acc) empty lst
